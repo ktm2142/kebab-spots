de5e41f8742fe206d009a0fd5452a5874842affd
ktm2142 <rghnmju@gmail.com>
2024-10-05 10:12:02 +0300

    language issue continues 4

--- kebab_app/src/settings.py
+++ kebab_app/src/settings.py
@@ -182,7 +182,7 @@ SOCIALACCOUNT_PROVIDERS = {
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'uk'
 
 LANGUAGES = [
     ('en', _('English')),
