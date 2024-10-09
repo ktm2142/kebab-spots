25137f7fc97b1db9893c705219be1f7e0a3c88a0
ktm2142 <rghnmju@gmail.com>
2024-10-09 13:07:43 +0300

    fixing points and recipes display at user profile page

--- kebab_app/accounts/views.py
+++ kebab_app/accounts/views.py
@@ -14,7 +14,7 @@ class CustomLoginView(LoginView):
 
     def get(self, request, *args, **kwargs):
         if 'google' in request.GET:
-            return redirect(reverse('google_login'))  # Змінено на 'google_login'
+            return redirect(reverse('google_login'))
         return super().get(request, *args, **kwargs)
 
 class CustomLogoutView(LogoutView):
@@ -24,21 +24,24 @@ class CustomLogoutView(LogoutView):
 class UserKebabSpotsView(LoginRequiredMixin, ListView):
     template_name = 'accounts/user_kebab_spots.html'
     context_object_name = 'kebab_spots'
-    queryset = (KebabSpot.objects
+
+    def get_queryset(self):
+        return (KebabSpot.objects
+                .filter(created_by=self.request.user)
                 .select_related('created_by')
                 .annotate(
-                    comments_count=Count('comments'),
-                    avg_rating=Avg('ratings__value'))
-                .order_by('-created_at')
-                )
+            comments_count=Count('comments'),
+            avg_rating=Avg('ratings__value'))
+                .order_by('-created_at'))
 
 
 class UserRecipesView(LoginRequiredMixin, ListView):
     template_name = 'accounts/user_recipes.html'
     context_object_name = 'recipes'
-    queryset = (Recipe.objects
+
+    def get_queryset(self):
+        return (Recipe.objects
+                .filter(author=self.request.user)
                 .select_related('author')
-                .annotate(
-                    avg_rating=Avg('recipe_ratings__value'))
-                .order_by('-created_at')
-                )
+                .annotate(avg_rating=Avg('recipe_ratings__value'))
+                .order_by('-created_at'))
